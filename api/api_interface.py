eb8f2418998a4c70ef78cd0b0ffe4524c28acf53
anveshdange <anveshdange19@gmail.com>
2023-02-09 16:53:12 +0530

    Added heart Machine learning model. also added code for heart api post method.

--- api/api_interface.py
+++ api/api_interface.py
@@ -11,9 +11,10 @@ load_dotenv()
 
 # importing all the environment variables here 
 KIDNEY_MODEL = os.environ["KIDNEY"]
-
+print(type(KIDNEY_MODEL))
 # inheriting the model schemas from another python file 
 from .model_schemas import kidney
+from .model_schemas import heart
 
 # here we are defining the API structure 
 app = FastAPI() # created an object for the FastAPI class 
@@ -23,5 +24,25 @@ app = FastAPI() # created an object for the FastAPI class
 # this is the kidney machine learning endpoint 
 @app.post("/kidney")
 async def kidney(data : kidney):
-    kidney_model : pickle = pickle.load(open(KIDNEY_MODEL, "rb")) 
-    return data 
+    os.chdir(r'C:\Users\admin\Desktop\Technex-2023_AI_Bytes\model_binaries')
+    infile = open("kidney.ai",'rb')
+    kidney_model = pickle.load(infile)
+    d = [[data.bp, data.sg, data.al, data.su, data.rbc, data.pc, data.pcc]]
+    prediction = kidney_model.predict(d)[0]
+    if prediction == 0: res = "No cancer"
+    else: res = "Cancer"
+    infile.close()
+    return {"Prediction":res}
+
+# this is the heart machine learning endpoint
+@app.post("/heart")
+async def heart(data: heart ): 
+    os.chdir(r'C:\Users\admin\Desktop\Technex-2023_AI_Bytes\model_binaries')
+    infile = open("heart.ai", "rb")
+    heart_model = pickle.load(infile)
+    heart_data = [[data.cp, data.trestbps, data.chol, data.fbs, data.restecg, data.thalach, data.exang]]
+    prediction = heart_model.predict(heart_data)[0]
+    infile.close()
+    if prediction == 0 : res = "You do not have blood cancer"
+    else : res = "You have blood cancer"
+    return {"Prediction": res}
--- api/model_schemas.py
+++ api/model_schemas.py
@@ -11,6 +11,17 @@ class kidney(BaseModel):
     pc :  float  
     pcc : float 
 
+class heart(BaseModel):
+    cp: int
+    trestbps: int
+    chol: int
+    fbs: int
+    restecg: int
+    thalach: int
+    exang: int
+
+
 # variable for exporting the data 
 # kidney_instance = kidney()  # created an object of the class kidney
+
 
